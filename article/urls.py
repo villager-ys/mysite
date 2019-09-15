58db8af7104a6edee1468ae8f671a9386f2b0500
yuanshuai <1179325921@qq.com>
2019-09-15 21:18:50 +0800

    first commit

--- article/urls.py
+++ article/urls.py
@@ -13,13 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
+from django.urls import path
+from article import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index),
-    path('article/', include('article.urls'), name="文章详情"),
+    path('<int:article_id>', views.article_detail, name="文章详情"),
+    path('', views.article_list, name='文章list'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, get_object_or_404, get_list_or_404
+from django.http import HttpResponse, Http404
+from .models import Article
+
 
 # Create your views here.
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    context = {'article': article}
+    return render_to_response("article_detail.html", context)
+
+
+def article_list(request):
+    articles = get_list_or_404(Article)
+    context = {'articles': articles}
+    return render_to_response('article_list.html', context)
