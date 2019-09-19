4c06fb45c2dacf3c78e1cb86433f068284985a29
yuanshuai <1179325921@qq.com>
2019-09-19 10:42:56 +0800

    个人博客

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Blog, BlogType
 
 # Create your views here.
 def blog_list(request):
-    content = {'blogs': Blog.objects.all()}
+    content = {'blogs': Blog.objects.all(), 'blog_types': BlogType.objects.all()}
     return render_to_response('blog_list.html', content)
 
 
@@ -15,5 +15,6 @@ def blog_detail(request, blog_id):
 
 def blog_with_type(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
-    content = {'blogs': Blog.objects.filter(blog_type=blog_type), 'blog_type': blog_type}
+    content = {'blogs': Blog.objects.filter(blog_type=blog_type), 'blog_type': blog_type,
+               'blog_types': BlogType.objects.all()}
     return render_to_response('blog_with_type.html', content)
