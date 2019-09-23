8d4d31ae514fe02784301cd1551a4c84b3500abf
yuanshuai <1179325921@qq.com>
2019-09-23 20:08:11 +0800

    缓存

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,3 +131,15 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CKEDITOR_UPLOAD_PATH = 'upload/'
+
+# 配置
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",  # 这里设定了本机的redis数据
+        # "LOCATION": "redis://:passwordpassword@47.193.146.xxx:6379/0", # 如果redis设置密码的话，需要以这种格式host前面是密码
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
--- mysite/views.py
+++ mysite/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render_to_response
 from django.contrib.contenttypes.models import ContentType
+from django.core.cache import cache
 from read_statistics.utils import get_seven_days_read_data, get_today_hot, get_yesterday_hot, get_7_days_hot
 from blog.models import Blog
 
 
 def home(request):
     blog_content_type = ContentType.objects.get_for_model(Blog)
-    dates, read_nums = get_seven_days_read_data(blog_content_type)
+    dates = cache.get('seven_days_read_data_dates')
+    read_nums = cache.get('seven_days_read_data_read_nums')
+    if dates is None or read_nums is None:
+        dates, read_nums = get_seven_days_read_data(blog_content_type)
+        cache.set_many({'seven_days_read_data_dates': dates, 'seven_days_read_data_read_nums': read_nums}, 3600)
     today_hot = get_today_hot(blog_content_type)
     yesterday_hot = get_yesterday_hot(blog_content_type)
-    hot_for_7 = get_7_days_hot()
+    hot_for_7 = cache.get('hot_for_7')
+    if hot_for_7 is None:
+        hot_for_7 = get_7_days_hot()
+        cache.set('hot_for_7', hot_for_7, 3600)
     context = {'dates': dates, 'read_nums': read_nums, 'today_hot': today_hot,
                'yesterday_hot': yesterday_hot, 'hot_for_7': hot_for_7}
     return render_to_response('home.html', context)
